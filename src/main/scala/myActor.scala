57d26ff4483b8dd7a8cd53d9c5a5958a0c3cb6cc
Julia-Kosowska <juliakosowska0@gmail.com>
2017-04-16 07:43:08 +0200

    clean html

--- src/main/scala/myActor.scala
+++ src/main/scala/myActor.scala
@@ -2,6 +2,7 @@ import java.net.URL
 
 import akka.actor.Actor.Receive
 import com.gargoylesoftware.htmlunit.html.HtmlPage
+import org.apache.commons.lang3.StringEscapeUtils
 import org.htmlcleaner.TagNode
 import weka.classifiers.trees.RandomTree
 import weka.core.pmml.jaxbbindings.True
@@ -40,72 +41,87 @@ class myActor extends Actor {
     var elem = rootNode.findElementByName("div", true)
     var lastParent = rootNode
     for (elem <- elements) {
-    //while(elem != null) {
+      //while(elem != null) {
       val classType = elem.getAttributeByName("class")
 
-//      // quick, well performing, but limited to yahoo
-//      if (classType != null && classType.contains("ads"))
-//        texts += ((elem.getText.toString, true))
-//      else if (classType != null && classType.contains("dd algo algo-sr"))
-//        texts += ((elem.getText.toString, false))
-
-
-      if (classType != null && classType.contains("ads" /*"scta reg searchCenterTopAds"*/)) {
-        // stories might be "dirty" with text like "'", clean it up
-        //val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
-        texts += ((elem.getText.toString, true)) //text
-        var level = 0
-        var parent = elem.getParent()
-        while (parent != null && parent.getName != "div") {
-          parent = parent.getParent()
-          level += 1
-        }
-        elem.removeFromTree()
-        //val siblings : Array[TagNode] = parent.getAllChildren().asScala.toArray//.getElementsByName("div", true)//getAllChildren().asScala
-        //        val siblings = parent.getChildTags()
-        if (parent != lastParent) {
-
-          var siblings = parent.getChildTags() //parent.getElementsByName("div", true)
-          val nlsiblings: ArrayBuffer[TagNode] = new ArrayBuffer[TagNode]
-          while (level > 0) {
-            for (sib: TagNode <- siblings) {
-              nlsiblings ++= sib.getChildTags()
-            }
-            level -= 1
-            siblings = nlsiblings.toArray
-          }
-          //siblings = siblings.filter(_.getName == "div")
-          for (sib: TagNode <- siblings) {
-            val classType = sib.getAttributeByName("class")
-            if (classType != null && classType.contains("ads")) {
-              texts += ((sib.getText.toString, true))
-            }
-            else {
-              texts += ((sib.getText.toString, false))
-            }
-          }
-          lastParent = parent
-          parent.removeFromTree()
-        }
+      // quick, well performing, but limited to yahoo
+      val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
+      if (classType != null && classType.contains("ads")) {
+        val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
+        texts += ((text, true))
       }
-      //elem = rootNode.findElementByName("div", true)
+      else if (classType != null && classType.contains("dd algo algo-sr")) {
+        val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
+        texts += ((text, false))
+      }
     }
 
-//    elem = rootNode
-//    while(elem != null){
-//      elem = rootNode.findElementByName("div", true)
-//      if(elem != null) {
-//        texts += ((elem.getText.toString, false))
-//        elem.removeFromTree()
-//      }
-//    }
 
-//    var parent = rootNode
-//    while(parent.getName != "div")
-//      parent = parent.findElementByName("div", true);
-//    texts += ((parent.getText.toString, false))
+    /*if (classType != null && classType.contains("ads" /*"scta reg searchCenterTopAds"*/)) {
+      // stories might be "dirty" with text like "'", clean it up
+      val text = StringEscapeUtils.unescapeHtml4(elem.getText.toString)
+      texts += ((text, true)) //text
+      var level = 0
+      var parent = elem.getParent()
+      while (parent != null && parent.getName != "div") {
+        parent = parent.getParent()
+        level += 1
+      }
+      elem.removeFromTree()
+      //val siblings : Array[TagNode] = parent.getAllChildren().asScala.toArray//.getElementsByName("div", true)//getAllChildren().asScala
+      //        val siblings = parent.getChildTags()
+      if (parent != lastParent) {
+
+        var siblings = parent.getChildTags() //parent.getElementsByName("div", true)
+        val nlsiblings: ArrayBuffer[TagNode] = new ArrayBuffer[TagNode]
+        while (level > 0) {
+          for (sib: TagNode <- siblings) {
+            nlsiblings ++= sib.getChildTags()
+          }
+          level -= 1
+          siblings = nlsiblings.toArray
+        }
+        //siblings = siblings.filter(_.getName == "div")
+        for (sib: TagNode <- siblings) {
+          val classType = sib.getAttributeByName("class")
+          if (classType != null && classType.contains("ads")) {
+            texts += ((sib.getText.toString, true))
+          }
+          else {
+            texts += ((sib.getText.toString, false))
+          }
+        }
+        lastParent = parent
+        parent.removeFromTree()
+      }
+    }
+    //elem = rootNode.findElementByName("div", true)
+    }*/
+
+    //    elem = rootNode
+    //    while(elem != null){
+    //      elem = rootNode.findElementByName("div", true)
+    //      if(elem != null) {
+    //        texts += ((elem.getText.toString, false))
+    //        elem.removeFromTree()
+    //      }
+    //    }
+
+    //    var parent = rootNode
+    //    while(parent.getName != "div")
+    //      parent = parent.findElementByName("div", true);
+    //    texts += ((parent.getText.toString, false))
     return texts.toList //.filter(storyContainsDesiredPhrase(_)).toList
   }
+
+  //var stories = new ListBuffer[String]
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _ => println("huh?")
+      var stories = getDivTextsFromUrl("https://search.yahoo.com/search;?p=shoes")//("file:F:\\uni 17.04\\scala\\SAG-WEDT\\example_sites\\text ads sites\\shoes - Yahoo Search Results.htm")
+      stories.foreach(println);
+  }
+}
 
   //var stories = new ListBuffer[String]
   def receive = {
